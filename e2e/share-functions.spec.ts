5dcdd8a65bacc73fa525918d811c8ec7d0a5e1bb
canbax <yusufsaidcanbaz@gmail.com>
2025-01-09 17:39:57 +0300

    divide test file

---
+++ e2e/share-functions.spec.ts
@@ -0,0 +1,52 @@
+import { expect, chromium } from '@playwright/test';
+import {
+  expectVisiblePrayingTimesBanner,
+  readTextFromClipboard,
+  selectLocation,
+  test,
+} from './test-utils';
+
+test('should share a valid URL to praying times', async ({ page }) => {
+  await selectLocation(page);
+  await expect(page.getByTestId('remaining-time')).toBeVisible();
+
+  await page.getByTestId('share-btn').click();
+  await page.getByTestId('copy-link-btn').click();
+
+  const clipboardText = await readTextFromClipboard(page);
+  const browser = await chromium.launch();
+  // Create a new incognito browser context
+  const context = await browser.newContext();
+  // Create a new page inside context.
+  const page2 = await context.newPage();
+  await page2.goto(clipboardText);
+
+  await expectVisiblePrayingTimesBanner(page2);
+});
+
+test('should be able to pop up share in app', async ({ page }) => {
+  await selectLocation(page);
+  await expect(page.getByTestId('remaining-time')).toBeVisible();
+
+  await page.getByTestId('share-btn').click();
+  await page.getByTestId('share-in-app-btn').click();
+});
+
+test('should be able to embed times widget', async ({ page }) => {
+  await selectLocation(page);
+  await expect(page.getByTestId('remaining-time')).toBeVisible();
+
+  await page.getByTestId('share-btn').click();
+  await page.getByTestId('embed-btn').click();
+
+  await expect(page.getByRole('alert').getByText('Embed to your website')).toBeVisible();
+
+  await expect(page.locator('iframe')).toBeVisible();
+  await page.getByRole('button', { name: 'Parameters' }).click();
+  await page.getByText('Is show hijri date?').click();
+  await page.getByTestId('copy-iframe-code-btn').click();
+
+  const clipboardText = await readTextFromClipboard(page);
+  expect(clipboardText).toContain('iframe');
+  expect(clipboardText).toContain('isShowHijri=0');
+});
--- e2e/times-page.spec.ts
+++ e2e/times-page.spec.ts
@@ -1,10 +1,5 @@
-import { expect, Page, chromium } from '@playwright/test';
-import {
-  expectVisiblePrayingTimesBanner,
-  readTextFromClipboard,
-  selectLocation,
-  test,
-} from './test-utils';
+import { expect, Page } from '@playwright/test';
+import { expectVisiblePrayingTimesBanner, selectLocation, test } from './test-utils';
 
 async function getVisibleDate(page: Page): Promise<Date> {
   const text = await page.getByTestId('date-str').textContent();
@@ -64,49 +59,4 @@ test('should be able to change location', async ({ page }) => {
   await selectLocation(page, false, 'İstanbul', 'İstanbul, İstanbul');
   await page.getByLabel('Close').click();
   await page.getByRole('button', { name: 'İstanbul' }).click();
-});
-
-test('should share a valid URL to praying times', async ({ page }) => {
-  await selectLocation(page);
-  await expect(page.getByTestId('remaining-time')).toBeVisible();
-
-  await page.getByTestId('share-btn').click();
-  await page.getByTestId('copy-link-btn').click();
-
-  const clipboardText = await readTextFromClipboard(page);
-  const browser = await chromium.launch();
-  // Create a new incognito browser context
-  const context = await browser.newContext();
-  // Create a new page inside context.
-  const page2 = await context.newPage();
-  await page2.goto(clipboardText);
-
-  await expectVisiblePrayingTimesBanner(page2);
-});
-
-test('should be able to pop up share in app', async ({ page }) => {
-  await selectLocation(page);
-  await expect(page.getByTestId('remaining-time')).toBeVisible();
-
-  await page.getByTestId('share-btn').click();
-  await page.getByTestId('share-in-app-btn').click();
-});
-
-test('should be able to embed times widget', async ({ page }) => {
-  await selectLocation(page);
-  await expect(page.getByTestId('remaining-time')).toBeVisible();
-
-  await page.getByTestId('share-btn').click();
-  await page.getByTestId('embed-btn').click();
-
-  await expect(page.getByRole('alert').getByText('Embed to your website')).toBeVisible();
-
-  await expect(page.locator('iframe')).toBeVisible();
-  await page.getByRole('button', { name: 'Parameters' }).click();
-  await page.getByText('Is show hijri date?').click();
-  await page.getByTestId('copy-iframe-code-btn').click();
-
-  const clipboardText = await readTextFromClipboard(page);
-  expect(clipboardText).toContain('iframe');
-  expect(clipboardText).toContain('isShowHijri=0');
 });
